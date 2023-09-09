97b88ac5594cb40fe666a24ed469d0fc7e143751
Robert-Ro <52750222+Robert-Ro@users.noreply.github.com>
2023-09-09 19:48:40 +0800

    Initial commit

---
+++ app2/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div
+      style={{
+        margin: '10px',
+        padding: '10px',
+        textAlign: 'center',
+        backgroundColor: 'cyan',
+      }}
+    >
+      <h1>App 2</h1>
+      <span>11</span>
+    </div>
+  );
+};
+
+export default App;
